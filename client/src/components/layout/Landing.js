86fd52324a47fa7597f27708534da2ede207499e
Max <maxdejong6@gmail.com>
2020-10-25 20:30:56 +1100

    Login and register Components
    front end

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -7,15 +7,15 @@ export const Landing = () => {
       <div className="dark-overlay">
         <div className="landing-inner">
           <h1 className="x-large">Developer Connector</h1>
-          <p class="lead">
+          <p className="lead">
             Create a developer profile/portfolio, share posts and get help from
             other developers
           </p>
           <div className="buttons">
-            <Link to="/register" class="btn btn-primary">
+            <Link to="/register" className="btn btn-primary">
               Sign Up
             </Link>
-            <Link to="/login" class="btn btn-light">
+            <Link to="/login" className="btn btn-light">
               Login
             </Link>
           </div>
